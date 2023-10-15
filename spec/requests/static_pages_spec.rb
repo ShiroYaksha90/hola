bd3338059f43c22fcde18a9e2ab88cc3d9015806
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-15 03:25:11 +0300

    Fix linter issues

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /static_pages/home" do
-    it "should render a successfull response" do
+RSpec.describe 'StaticPages', type: :request do
+  describe 'GET /static_pages/home' do
+    it 'should render a successfull response' do
       get static_pages_home_path
       expect(response).to have_http_status(200)
     end
@@ -13,8 +13,8 @@ RSpec.describe "StaticPages", type: :request do
     end
   end
 
-  describe "GET /static_pages/help" do
-    it "should render a successfull response" do
+  describe 'GET /static_pages/help' do
+    it 'should render a successfull response' do
       get static_pages_help_path
       expect(response).to have_http_status(200)
     end
@@ -25,8 +25,8 @@ RSpec.describe "StaticPages", type: :request do
     end
   end
 
-  describe "GET /static_pages/about" do
-    it "should render a successfull response" do
+  describe 'GET /static_pages/about' do
+    it 'should render a successfull response' do
       get static_pages_about_path
       expect(response).to have_http_status(200)
     end
