b7244fb0704b03ceb6860feda4d8bfe4401f891e
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-15 23:50:11 +0300

    Add title spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe 'StaticPages', type: :request do
       get static_pages_home_path
       expect(response.body).to include('Welcome to our website!')
     end
+
+    it 'should have the right title' do
+      get static_pages_home_path
+      expect(response.body).to include('<title>Home | Hola App</title>')
+    end
   end
 
   describe 'GET /static_pages/help' do
@@ -23,6 +28,11 @@ RSpec.describe 'StaticPages', type: :request do
       get static_pages_help_path
       expect(response.body).to include('Welcome to the help page')
     end
+
+    it 'should have the right title' do
+      get static_pages_help_path
+      expect(response.body).to include('<title>Help | Hola App</title>')
+    end
   end
 
   describe 'GET /static_pages/about' do
@@ -31,9 +41,15 @@ RSpec.describe 'StaticPages', type: :request do
       expect(response).to have_http_status(200)
     end
 
-    it "should have the content 'Help'" do
+    it "should have the content 'About'" do
       get static_pages_about_path
       expect(response.body).to include('About Us')
     end
+
+    it 'should have the right title' do
+      get static_pages_about_path
+      expect(response.body).to include('<title>About | Hola App</title>')
+    end
   end
 end
+
