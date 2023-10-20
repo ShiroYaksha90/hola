b6798b64e8d4c4aad86887536aa73e1f8e312f44
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-21 02:18:47 +0300

    Add users view specs

---
+++ spec/views/users/new.html.erb_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'user/new.html.erb', type: :feature do
+  scenario 'should have the right title' do
+    visit '/signup'
+    expect(page).to have_content('Sign up')
+    expect(page).to have_title('Sign up | Hola App')
+    expect(page).to have_selector('p', text: 'This will be a signup page for new users.')
+  end
+end
