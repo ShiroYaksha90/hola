e10a5c720c66c13f761b8e9d59e67998bc765e22
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-10-21 23:39:34 +0300

    Add balnk & length validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new(name: "Example User", email: "example123@example.com")
+  end
+  describe "validations" do
+    it "is valid with valid attributes" do
+      expect(@user).to be_valid
+    end
+    it "is not valid without a name" do
+      @user.name = nil
+      expect(@user).to_not be_valid
+    end
+    it "is not valid without an email" do
+      @user.email = nil
+      expect(@user).to_not be_valid
+    end
+    it "is not valid with a name longer than 50 characters" do
+      @user.name = "a" * 51
+      expect(@user).to_not be_valid
+    end
+    it "is not valid with email longer than 255 characters" do
+      @user.email = 'a' * 244 + '@example.com'
+      expect(@user).to_not be_valid
+    end
+  end
 end
