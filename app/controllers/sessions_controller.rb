db9e4c4600a8bbd1b9151f67650a55e4b7240eb8
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-08 21:31:25 +0300

    Authenticate user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,13 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    render 'new', status: :unprocessable_entity
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Log the user in and redirect to the user's show page.
+    else
+      #Create an error message.
+      render 'new', status: :unprocessable_entity
+    end
   end
 
   def destroy
