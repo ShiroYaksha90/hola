53eee071fa9ebf71cb9b576067329614d602d1bb
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-09 22:18:24 +0300

    Add specs for failed user login

---
+++ spec/requests/users_logins_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe "UsersLogins", type: :request do
+  describe "GET /users_logins" do
+    it "works! (now write some real specs)" do
+      get login_path
+      render_template 'sessions/new'
+      post login_path, params: { session: { email: "", password: "" } }
+      expect(response).to be_unprocessable
+      render_template 'sessions/new'
+      expect(flash.empty?).to be_falsey
+      get root_path
+      expect(flash.empty?).to be_truthy
+    end
+  end
+end
