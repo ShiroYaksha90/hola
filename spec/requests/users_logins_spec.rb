b63daca7c648154decfac3097f0eb113967c1c78
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-10 21:09:03 +0300

    Fix linter issues

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe "UsersLogins", type: :request do
-  describe "GET /users_logins" do
-    it "works! (now write some real specs)" do
+RSpec.describe 'UsersLogins', type: :request do
+  describe 'GET /users_logins' do
+    it 'works! (now write some real specs)' do
       get login_path
       render_template 'sessions/new'
-      post login_path, params: { session: { email: "", password: "" } }
+      post login_path, params: { session: { email: '', password: '' } }
       expect(response).to be_unprocessable
       render_template 'sessions/new'
       expect(flash.empty?).to be_falsey
