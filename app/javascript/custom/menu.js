06412789443b8725a36bfbcce761a47d64653667
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-12 16:04:52 +0300

    Refactor & make it DRY

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,8 +1,13 @@
-document.addEventListener("turbo:load", function(){
-    let account = document.querySelector("#account")
-    account.addEventListener("click", function(event){
+const addToggleListener = (selectedId, menuId, toggleClass) => {
+    let selectedElement = document.querySelector(`#${selectedId}`)
+    selectedElement.addEventListener('click', (event) => {
         event.preventDefault()
-        let menu = document.querySelector("#dropdown-menu")
-        menu.classList.toggle("active")
+        let menu = document.querySelector(`#${menuId}`)
+        menu.classList.toggle(toggleClass)
     })
+}
+
+document.addEventListener("turbo:load", ()=> {
+    addToggleListener("hamburger", "navbar-menu", "collapse")
+    addToggleListener("account", "dropdown-menu", "active")
 })
