09c63acd97bd698b44dacbb42bc4c51891aad8d0
ShiroYaksha90 <aljazaeribasim@gmail.com>
2023-11-12 18:37:08 +0300

    Use users fixture for valid login spec

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersLogins', type: :request do
+  fixtures :users
   describe 'GET /users_logins' do
     it 'works! (now write some real specs)' do
       get login_path
@@ -11,6 +12,20 @@ RSpec.describe 'UsersLogins', type: :request do
       expect(flash.empty?).to be_falsey
       get root_path
       expect(flash.empty?).to be_truthy
+    end
+  end
+
+  describe 'login with valid information' do
+    it 'login with valid information' do
+      @user = users(:basim)
+      get login_path
+      post login_path, params: { session: { email: @user.email, password: 'password' } }
+      expect(response).to redirect_to(@user)
+      follow_redirect!
+      expect(response).to render_template('users/show')
+      expect(response.body).to_not include('Login')
+      expect(response.body).to include('Logout')
+      expect(response.body).to include('Profile')
     end
   end
 end
